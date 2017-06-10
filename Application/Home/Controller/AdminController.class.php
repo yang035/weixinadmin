132224a2420d4954ed92404d40e91e2bcd5f9bc0
yango35 <ernest96@yeah.net>
2017-06-11 00:15:57 +0800

    first commit

---
+++ Application/Home/Controller/AdminController.class.php
@@ -0,0 +1,27 @@
+<?php
+// +----------------------------------------------------------------------
+// | Soft [ 简单 高效 卓越 ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2016 http://www.soft150.com All rights reserved.
+// +----------------------------------------------------------------------
+// | Author: jry <598821125@qq.com>
+// +----------------------------------------------------------------------
+namespace Home\Controller;
+
+use Think\Controller;
+
+/**
+ * 跳转到后台控制器
+ * @author jry <598821125@qq.com>
+ */
+class AdminController extends Controller
+{
+    /**
+     * 自动跳转到后台入口文件
+     * @author jry <598821125@qq.com>
+     */
+    public function index()
+    {
+        redirect(C('HOME_PAGE') . '/admin.php');
+    }
+}
